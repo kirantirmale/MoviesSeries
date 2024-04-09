2fa887f5a7c53255927d9a0fbd066a73e69d82b2
Kiran Tirmale <127860308+kirantirmale@users.noreply.github.com>
2024-04-09 18:34:21 +0530

    commit

--- app/components/Title.jsx
+++ app/components/Title.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Title = ({ title, handleSearchInputChange }) => {
   return (
-    <header className='w-full bg-slate-700 top-16 fixed w-full  mob'>
+    <header className='w-full bg-slate-700 top-16 fixed w-full px-4  mob'>
       <nav className='flex  sm:flex-row justify-between items-center sm:px-16 '>
         <h1 className='text-2xl text-white font-semibold mb-2 sm:mb-0 sm:mr-4'>
           Popular {title}
