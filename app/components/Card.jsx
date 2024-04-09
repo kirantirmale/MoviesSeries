c8fb245fb750334fd88278ee1cf29ac933ab5bdc
Kiran Tirmale <127860308+kirantirmale@users.noreply.github.com>
2024-04-09 17:06:23 +0530

    commit

--- app/components/Card.jsx
+++ app/components/Card.jsx
@@ -9,7 +9,7 @@ export default function Home() {
             <div className="flex flex-col sm:flex-row p-4 sm:p-10  ancjor">
               <div className="text-center px-4 mb-4 sm:mb-0 ">
               
-                  <Link href="/Series" className="block max-w-sm py-16 px-8 sm:py-24 sm:px-12 bg-dark bgone border  border-gray-200 shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                  <Link href="/Series" className="block max-w-sm py-16 px-8 sm:py-24 sm:px-12 bg-dark bgone border  border-gray-200 shadow  dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
                     <h5 className="mb-2 text-lg sm:text-2xl font-bold text-gray-100 dark:text-white">SERIES</h5>
                   </Link>
             
@@ -18,7 +18,7 @@ export default function Home() {
 
               <div className="text-center px-4 mb-4 sm:mb-0">
                 
-                  <Link href="/Movies" className="block max-w-sm py-16 px-8 sm:py-24 sm:px-12 bg-white bgone border border-gray-200 shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                  <Link href="/Movies" className="block max-w-sm py-16 px-8 sm:py-24 sm:px-12 bg-white bgone border border-gray-200 shadow  dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
                     <h5 className="mb-2 text-lg sm:text-2xl font-bold text-gray-100 dark:text-white">MOVIES</h5>
                   </Link>
               
